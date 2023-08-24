dce36301c8ed2cab8f05b4af8e1a101889b02613
Muhammad Awais Shafi <awaisshafi6164@gmail.com>
2023-08-24 14:16:17 +0500

    Route is successfully implement using google map

--- app/src/main/java/com/example/location_tracking_app/views/HomeFragment.kt
+++ app/src/main/java/com/example/location_tracking_app/views/HomeFragment.kt
@@ -16,12 +16,14 @@ import com.example.location_tracking_app.viewmodel.AuthViewModel
 import com.example.location_tracking_app.viewmodel.MapViewModel
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-
 import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Geocoder
+import android.location.Address
 import android.os.AsyncTask
 import android.util.Log
+import android.widget.EditText
 import androidx.core.app.ActivityCompat
 import com.example.location_tracking_app.repository.GoogleMapDTO
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -33,6 +35,7 @@ import com.google.firebase.database.*
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.io.IOException
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -42,7 +45,10 @@ class HomeFragment : Fragment() {
     private lateinit var mapViewModel: MapViewModel
     private lateinit var navController: NavController
     private lateinit var signOutBtn: Button
+    private lateinit var getDirectionBTN: Button
     private lateinit var userTV: TextView
+    private lateinit var startLocET: EditText
+    private lateinit var endLocET: EditText
 
     //
     //
@@ -54,8 +60,6 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         viewModel = ViewModelProvider(
             this,
             ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
@@ -78,7 +82,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
@@ -125,22 +128,138 @@ class HomeFragment : Fragment() {
 
             googleMap.isMyLocationEnabled = true
 
-            val location1 = LatLng(33.66263618397245, 73.05473119595356)
-            googleMap.addMarker(MarkerOptions().position(location1).title("RWR Office"))
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,12f))
+            val pakistan = LatLng(30.3753, 69.3451)
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pakistan,5f))
 
-            val location2 = LatLng(33.66401100214133, 73.04693296520178)
-            googleMap.addMarker(MarkerOptions().position(location2).title("Railway Station"))
+//            val location1 = LatLng(33.66263618397245, 73.05473119595356)
+//            googleMap.addMarker(MarkerOptions().position(location1).title("RWR Office"))
+//            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,12f))
 
-            Log.d("GoogleMap", "before URL")
-            val URL = getDirectionURL(location1, location2)
-            Log.d("GoogleMap", "URL : $URL")
-            GetDirection(URL).execute()
+//            val location2 = LatLng(33.66401100214133, 73.04693296520178)
+//            googleMap.addMarker(MarkerOptions().position(location2).title("Railway Station"))
+
+            startLocET = view.findViewById(R.id.startLocET)
+            endLocET = view.findViewById(R.id.endLocET)
+            getDirectionBTN = view.findViewById(R.id.getDirectionBtn)
+
+            getDirectionBTN.setOnClickListener {
+                val startLocationArea = startLocET.text.toString().trim()
+                val endLocationArea = endLocET.text.toString().trim()
+
+                if (startLocationArea.isNotEmpty() && endLocationArea.isNotEmpty()) {
+                    var startLocationOutside: LatLng? = null // Initialize with null
+                    var endLocationOutside: LatLng? = null   // Initialize with null
+
+                    getCoordinateStartLocation(startLocationArea) { startLocation ->
+                        if (startLocation != null) {
+                            startLocationOutside = startLocation
+                            // Move the rest of your code here that depends on startLocation
+                            getCoordinateEndLocation(endLocationArea) { endLocation ->
+                                if (endLocation != null) {
+                                    endLocationOutside = endLocation
+
+                                    val URL = getDirectionURL(startLocationOutside!!, endLocationOutside!!)
+                                    Log.d("GoogleMap", "URL : $URL")
+                                    GetDirection(URL).execute()
+                                } else {
+                                    toast("Something is wrong with the end location")
+                                }
+                            }
+                        } else {
+                            toast("Something is wrong with the start location")
+                        }
+                    }
+                } else {
+                    toast("Please enter a start or end location name")
+                }
+            }
+
+
+//            val URL = getDirectionURL(startLocation, location2)
+//            Log.d("GoogleMap", "URL : $URL")
+//            GetDirection(URL).execute()
+
+//            val URL = getDirectionURL(location1, location2)
+//            Log.d("GoogleMap", "URL : $URL")
+//            GetDirection(URL).execute()
         })
 
         //
         //
     }
+
+    private fun getCoordinateStartLocation(area: String, callback: (LatLng?) -> Unit) {
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+
+        Thread {
+            try {
+                val addresses: List<Address>? = geocoder.getFromLocationName(area, 1)
+                if (addresses != null && addresses.isNotEmpty()) {
+                    val address = addresses[0]
+                    val latitude = address.latitude
+                    val longitude = address.longitude  // Use longitude instead of latitude here
+
+                    requireActivity().runOnUiThread {
+//                        val message = "Latitude: $latitude\nLongitude: $longitude"
+//                        toast(message)
+
+                        val startLocation = LatLng(latitude, longitude)
+                        googleMap.addMarker(MarkerOptions().position(startLocation).title("Start Location"))
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(startLocation,10f))
+                        callback(startLocation) // Call the callback with the obtained location
+                    }
+                } else {
+                    requireActivity().runOnUiThread {
+                        toast("No results found for the given city")
+                        callback(null) // Call the callback with null when no results are found
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                requireActivity().runOnUiThread {
+                    toast("Error: ${e.message}")
+                    callback(null) // Call the callback with null in case of error
+                }
+            }
+        }.start()
+    }
+
+    private fun getCoordinateEndLocation(area: String, callback: (LatLng?) -> Unit) {
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+
+        Thread {
+            try {
+                val addresses: List<Address>? = geocoder.getFromLocationName(area, 1)
+                if (addresses != null && addresses.isNotEmpty()) {
+                    val address = addresses[0]
+                    val latitude = address.latitude
+                    val longitude = address.longitude  // Use longitude instead of latitude here
+
+                    requireActivity().runOnUiThread {
+//                        val message = "Latitude: $latitude\nLongitude: $longitude"
+//                        toast(message)
+
+                        val endLocation = LatLng(latitude, longitude)
+                        googleMap.addMarker(MarkerOptions().position(endLocation).title("End Location"))
+                        //googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(endLocation,10f))
+                        callback(endLocation) // Call the callback with the obtained location
+                    }
+                } else {
+                    requireActivity().runOnUiThread {
+                        toast("No results found for the given city")
+                        callback(null) // Call the callback with null when no results are found
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                requireActivity().runOnUiThread {
+                    toast("Error: ${e.message}")
+                    callback(null) // Call the callback with null in case of error
+                }
+            }
+        }.start()
+    }
+
 
     fun getDirectionURL(origin:LatLng,dest:LatLng) : String{
         val apiKey = getString(R.string.map_api_key)
@@ -152,7 +271,9 @@ class HomeFragment : Fragment() {
             val client = OkHttpClient()
             val request = Request.Builder().url(url).build()
             val response = client.newCall(request).execute()
-            val data = response.body()!!.toString()
+            val responseBody = response.body()
+            val data = responseBody?.string()
+            Log.d("GoogleMap", "Response Data: $data")
             val result = ArrayList<List<LatLng>>()
             try {
 
